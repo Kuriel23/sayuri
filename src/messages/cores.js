16b50a9bcaa4ec82177b3e20dfdc1827fb4f4dba
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-03-15 02:04:37 +0000

    feat: new text for colors

--- src/messages/cores.js
+++ src/messages/cores.js
@@ -7,7 +7,7 @@ module.exports = async (client, message) => {
     .setColor("#19b5ff")
     .setTitle("⌗ 〣  Escolha sua cor!")
     .setDescription(
-      "Boas-vindas ao canal de cores, aqui você poderá escolher uma cor de sua preferencia para decorar seu perfil! \n\n> Nossas cores são organizadas pelos números 1, 2, 3. O número 1 representa os tons padrões das cores, o 2 os tons pastéis, e os 3 os tons neons.\n\n(Obs: Temos 2 paletas do tom azul a primeira são tons mais claros e a segunda tons mais escuros.)\n\n__tambem não é obrigatorio, isso vai de você!__"
+      "*• Boas-vindas ao canal de cores, aqui você poderá escolher uma cor de sua preferencia para decorar seu perfil!*\n\n> ✦ ୨ Em nossas cores possuímos três tons de cada, exceto as cores extras que não possuem três.  Temos os tons de cores padrões, pastéis e neons.\n\n__também não é obrigatorio, isso vai de você!__"
     )
     .setImage("https://i.imgur.com/qCgpXyv.jpeg");
   const row = new discord.ActionRowBuilder().addComponents(
@@ -15,18 +15,6 @@ module.exports = async (client, message) => {
       .setCustomId("padrões")
       .setPlaceholder("Tons padrões")
       .addOptions([
-        {
-          label: "Branco",
-          value: "Branco",
-        },
-        {
-          label: "Cinza",
-          value: "Cinza",
-        },
-        {
-          label: "Preto",
-          value: "Preto",
-        },
         {
           label: "Vermelho",
           value: "Vermelho",
